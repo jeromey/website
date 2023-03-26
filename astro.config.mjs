ddb07b1921cf3889ce67ff22cf000be51821c04e
jeromey <jerome.salimao@gmail.com>
2023-03-26 17:46:01 +1100

    added deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jeromesalimao.com',
   integrations: [mdx(), sitemap(), react()]
 });
